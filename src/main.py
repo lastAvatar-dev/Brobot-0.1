5834d517362b799d926ddfdcd19b13ccad52fc40
lastAvatar-dev <70703296+lastAvatar-dev@users.noreply.github.com>
2021-02-07 16:52:04 +0700

    init

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import discord
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+        if message.author.bot:
+            return
+        await message.channel.send("Hello from Bot")
+
+client = MyClient()
+client.run(os.getenv('TOKEN'))
